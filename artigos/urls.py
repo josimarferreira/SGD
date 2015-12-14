2b0b24a5f2145c7d7cf4d0a8f5b4d230636d5d32
Josimar da Cunha Ferreira <josimarcf@gmail.com>
2015-12-14 18:46:48 -0300

    Adicionados Visualização, edição e exclusao de artigos

--- artigos/urls.py
+++ artigos/urls.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 from django.conf.urls import include, url, patterns
-from django.views.generic import TemplateView
-from .views import CreateArtigoView, ListaArtigos
+from .views import CreateArtigoView, ListaArtigos, MostraArtigo, DeletaArtigo, EditaArtigo
 
 urlpatterns = patterns('',
     url(r'^novo/$', CreateArtigoView.as_view(), name='CriarArtigo'),
+    url(r'^mostra(?P<nr_contato>\d+)/$', MostraArtigo, name='MostraArtigo'),
+    url(r'^edita(?P<nr_contato>\d+)/$', EditaArtigo, name='EditaArtigo'),
+    url(r'^deleta(?P<nr_contato>\d+)/$', DeletaArtigo, name='DeletaArtigo'),
     url(r'^$', ListaArtigos.as_view(template_name='artigos.html'), name='lista'),
 )
--- artigos/views.py
+++ artigos/views.py
@@ -1,9 +1,16 @@
 # coding: utf-8
+from django.template import RequestContext
 
 from django.views.generic import TemplateView, ListView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from artigos.forms import ArtigosForm
 from artigos.models import Artigos
+
+
+class ListaArtigos(ListView):
+    template_name = 'artigos.html'
+    model = Artigos
+    context_object_name = 'artigo'
 
 
 class CreateArtigoView(TemplateView):
@@ -23,7 +30,33 @@ class CreateArtigoView(TemplateView):
         return self.render_to_response(context)
 
 
-class ListaArtigos(ListView):
-    template_name = 'artigos.html'
-    model = Artigos
-    context_object_name = 'artigo'
+def EditaArtigo(request, nr_contato):
+    artigo = get_object_or_404(Artigos, pk=nr_contato)
+    if request.method == 'POST':
+        form = ArtigosForm(request.POST, request.FILES, instance=artigo)
+        if form.is_valid():
+            form.save()
+            return render_to_response('salvo.html', {})
+    else:
+        form = ArtigosForm(instance=artigo)
+    return render_to_response("edita_artigo.html", {'form': form}, context_instance=RequestContext(request))
+
+
+
+def DeletaArtigo(request, nr_contato):
+    artigo = get_object_or_404(Artigos, pk=nr_contato)
+    form = ArtigosForm(instance=artigo)
+
+    if request.method == 'POST':
+        artigo.delete()
+        return render_to_response('artigo_deletado.html', {})
+    else:
+        return render_to_response("deleta_artigo.html", {'artigo': artigo}, RequestContext(request))
+
+
+def MostraArtigo(request, nr_contato):
+    artigo = get_object_or_404(Artigos, pk=nr_contato)
+    return render_to_response("mostra_artigo.html", {'artigo': artigo})
+
+
+
